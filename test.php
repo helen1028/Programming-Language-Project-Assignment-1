4b24a1d0539754f5f67633890fd150bfe46ada19
helen1028 <90227977+helen1028@users.noreply.github.com>
2021-09-16 12:16:36 -0400

    string-int-float-array-dictionary

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+    //boolean
+    $a = true; 
+    $b = false; 
+    
+    var_dump($a);
+    var_dump($b);
+    
+    //int
+    $z = 7;
+    var_dump($z);
+
+    //float
+    $c = 6.13;
+    var_dump($c);
+
+    //string
+    $d = "I can do it! I'm good!";
+    var_dump($d);
+
+    //array/list
+    $e = array("you", "are", "the", "best","thank","you");
+    var_dump($e)
+
+    //dictionary
+    $f = [
+        'bts' => 7,
+        'v' => 1995,
+        'suga' => 1993,
+    ];
+    var_dump($f);
+
+?>
